a5148088bc9c8149b0f6ed15e6d1f9c907f010d9
EJAyarma <johnayarmahe@gmail.com>
2022-06-02 23:32:52 +0000

    Correct optional task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,23 +3,23 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
-    ag = len(argv) - 1
-    if ag != 3:
+    ag = len(argv)
+    if ag != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
     operator = argv[2]
     b = int(argv[3])
-    if operator not in "**+/-":
+
+    if operator == "*":
+        c = mul(a, b)
+    elif operator == "+":
+        c = add(a, b)
+    elif operator == "-":
+        c = sub(a, b)
+    elif operator == "/":
+        c = div(a, b)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    c = 0
-    if operator == '*':
-        c = a * b
-    elif operator == '+':
-        c = a + b
-    elif operator == '-':
-        c = a - b
-    elif operator == '/':
-        c = a / b
     print("{:d} {:s} {:d} = {}".format(a, operator, b, c))
