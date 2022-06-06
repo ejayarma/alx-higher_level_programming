65d9ff61dc97455e95df192369c1c17e20de2ad7
EJAyarma <johnayarmahe@gmail.com>
2022-06-06 17:04:41 +0000

    Correct task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def replace_in_list(my_list, idx, element):
+def element_at(my_list, idx):
     new_element = None
     if 0 <= idx < len(my_list):
         new_element = my_list[idx]
