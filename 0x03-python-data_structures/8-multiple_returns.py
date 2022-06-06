f8b20edaf2cc3478c6eb3098e5c7ed0445afef7a
EJAyarma <johnayarmahe@gmail.com>
2022-06-06 17:51:36 +0000

    Add task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    str_len = len(sentence)
+    first_char = None
+    if sentence:
+        first_char = sentence[0]
+    return (str_len, first_char)
