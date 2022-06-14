31d7b9cda11bd2dfd790a15df9e08c42ee10f425
EJAyarma <johnayarmahe@gmail.com>
2022-06-14 18:36:02 +0000

    Add correction for task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,6 +3,8 @@
 def weight_average(my_list=[]):
     total_sum = 0
     total_weight = 0
+    if not len(my_list):
+        return total_sum
     for t in my_list:
         score, weight = t
         total_weight += weight
