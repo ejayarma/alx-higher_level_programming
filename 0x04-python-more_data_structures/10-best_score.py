664e650d28803d6ea4c0c90ecd0344a4d8afd004
EJAyarma <johnayarmahe@gmail.com>
2022-06-14 19:01:39 +0000

    Correct task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@
 def best_score(a_dictionary):
     mk = None
     mv = 0
-    if a_dictionary is not None:
+    if isinstance(a_dictionary, dict) and a_dictionary is not None:
         for k, v in a_dictionary.items():
             if v > mv:
                 mk = k
