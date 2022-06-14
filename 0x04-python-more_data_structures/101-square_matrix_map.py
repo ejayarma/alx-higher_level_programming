a55d06f60f86b0488944b21e52ffaf08948cd519
EJAyarma <johnayarmahe@gmail.com>
2022-06-14 19:09:44 +0000

    Correct one advancecd task

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: [n * n for n in x], matrix))
+    return list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
