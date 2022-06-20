a916a4980d9ed1520d492bab728d3ff2f7f66b28
EJAyarma <johnayarmahe@gmail.com>
2022-06-20 13:09:15 +0000

    Attempt to correct task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,9 +6,9 @@ def safe_print_list(my_list=[], x=0):
         return print_count
     for i in range(x):
         try:
-            print(my_list[i], end='')
+            print(f'{my_list[i]}', end='')
             print_count += 1
-        except Exception:
+        except IndexError:
             pass
     print()
     return print_count
