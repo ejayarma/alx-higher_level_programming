5443e2540a3e5183df77c5c5fbf0ab1155c5a97a
EJAyarma <johnayarmahe@gmail.com>
2022-06-20 13:56:29 +0000

    Attempt correction of task 1

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,5 +10,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print_count += 1
         except ValueError:
             pass
+        except IndexError:
+            raise IndexError
     print()
     return print_count
