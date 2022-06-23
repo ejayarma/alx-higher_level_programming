bd13d81fab2d35c602526281b7f6284be73c466a
EJAyarma <johnayarmahe@gmail.com>
2022-06-23 05:21:43 +0000

    Add a lot of other tasks

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """Representation of square class"""
+
+    def __init__(self, size=0):
+
+        """Initialize a square.
+
+        Args:
+            size (int): square size.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
