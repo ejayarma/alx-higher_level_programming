fec4a0d823a00dd921ccf6202e814b02abcdf4c3
EJAyarma <johnayarmahe@gmail.com>
2022-06-23 05:16:30 +0000

    Correct tasks 1 and 0

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""Define a class called Square."""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize the Square.
+
+        Args:
+            size (int): size of the square.
+        """
+
         self.__size = size
