97d70148f99ef479e56b6506ec7a42a4115450f6
EJAyarma <johnayarmahe@gmail.com>
2022-06-23 05:25:40 +0000

    Correct a lot of other tasks

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,59 @@
 #!/usr/bin/python3
-Square = __import__('5-square').Square
+"""Defines a class Square"""
 
-my_square = Square(3)
-my_square.my_print()
 
-print("--")
+class Square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        self.size = size
 
-my_square.size = 10
-my_square.my_print()
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
 
-print("--")
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
 
-my_square.size = 0
-my_square.my_print()
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
-print("--")
+    def my_print(self):
+        """prints the square
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))
